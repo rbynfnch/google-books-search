e1f60b030f9c8d4069b4f404a2c908d38156f4c1
Robyn <rbynfnch@msn.com>
2019-07-22 20:30:19 -0600

    working on css

--- src/pages/SaveBooks.js
+++ src/pages/SaveBooks.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import Jumbotron from "../components/Jumbotron";
+import Header from "../components/Header";
 import { Container} from "../components/Grid";
 import SavedResult from "../components/SavedResult"
 
--- src/pages/SearchBooks.js
+++ src/pages/SearchBooks.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import Jumbotron from "../components/Jumbotron";
+import Header from "../components/Header";
 import { Container, Row, Col } from "../components/Grid";
 import SearchForm from "../components/SearchForm";
 import SearchResult from "../components/SearchResult"
@@ -67,8 +68,11 @@ class SearchBooks extends Component {
         return (
             <Container fluid>
                 <Jumbotron>
-                    <h1 className="text-white">Find Your Favorite Books with GoogleBook API</h1>
                 </Jumbotron>
+                <Header>
+                    <h1 className="header">Find Your Favorite Books with GoogleBook API</h1>
+
+                </Header>
                 <Container>
                     <Row>
                         <Col size="12">
